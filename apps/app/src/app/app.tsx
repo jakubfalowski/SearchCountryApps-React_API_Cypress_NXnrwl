9d76b5e5e712268c1feb70fd92367f57db7a2b17
jakubfalowski <kubafa99@gmail.com>
2022-03-30 11:31:13 +0200

    tutorial nx.dev connected to api

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import searchAPI from './searchAPI';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="app" />
+      <NxWelcome title="app" /><searchAPI />
       </div>
   );
 }
--- apps/app/src/app/searchAPI.tsx
+++ apps/app/src/app/searchAPI.tsx
@@ -1,12 +1,47 @@
-export function searchAPI(){
+import React, { useEffect, useState } from "react";
 
-    return (
-        <div id="content">
-        </div>
+interface Todo{
+    title: string;
+}
+
+export const searchAPI = () => {
+    const [todos, setTodos] = useState<Todo[]>([
+        {title: "Todo1"},
+        {title: "Todo2"},
+    ]);
+    useEffect(() => {
+        fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland')
+        .then((_) => _.json())
+        .then(setTodos);
+    }, []);
+    
+    function addTodo(){
+        fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland',{
+            method: 'POST',
+            body: '',
+        })
+        .then((_) => _.json())
+        .then((newTodo) => {
+            setTodos([...todos, newTodo]);
+        });
+    }
+    return(
+        <>
+            <h1>Todos</h1>
+            <ul>
+                {todos.map((t) => (
+                    <li className={'todo'}>{t.title}</li>
+                ))}
+            </ul>
+            <button id={'add-todo'} onClick={addTodo}>
+                Add Todo
+            </button>
+        </>
     );
-  }
+  };
   
   export default searchAPI;
   // AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g key
+  // https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland
 
   

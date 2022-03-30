681088e56372a85650a6b444b2921673e080c179
jakubfalowski <kubafa99@gmail.com>
2022-03-30 15:02:57 +0200

    data from google api without child

--- apps/app2/pages/comments/index.js
+++ apps/app2/pages/comments/index.js
@@ -4,22 +4,16 @@ function CommentsPage(){
     const [comments, setComments] = useState([])
 
     const fetchComments = async () => {
-        const response = await fetch('/api/comments')
+        const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland')
         const data = await response.json()
         setComments(data)
     }
     return(
         <div>
         <button onClick = {fetchComments}> Load comments </button>
-        {comments.map((comment) => {
-            return(
-                <div key={comment.id}>
-                {comment.id} {comment.text}
-                </div>
-            )
-        })}
+        {comments.kind}
         </div>
     )
 }
-
+// https://stackoverflow.com/questions/54229018/objects-are-not-valid-as-a-react-child-found-object-promise-if-you-meant-t
 export default CommentsPage

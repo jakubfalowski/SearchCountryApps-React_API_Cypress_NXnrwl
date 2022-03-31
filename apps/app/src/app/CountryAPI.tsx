1770912056e9952ea53e4f0983513c5d48bc6cb2
jakubfalowski <kubafa99@gmail.com>
2022-03-31 15:14:26 +0200

    countryAPI map

--- apps/app/src/app/CountryAPI.tsx
+++ apps/app/src/app/CountryAPI.tsx
@@ -1,37 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
-const LAUNCHES_QUERY = `
+const COUNTRIES_QUERY = `
 {
-        country(code: "PL") {
-          name
-          native
-          capital
-          emoji
-          currency
-          languages {
-            code
-            name
-          }
-        }
+  continent(code:"EU"){
+    code
+    name
+    countries{
+      name
+      capital
+    }
+  }
 }
   `
 
 export default function CountryAPI(){
-    const [launches, setLaunches] = React.useState([]);
+    const [countries, setCountries] = useState([] as any[]);
 
     React.useEffect(() => {
         fetch('https://countries.trevorblades.com/graphql',{
             method: "POST",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify ({ query: LAUNCHES_QUERY })
+            body: JSON.stringify ({ query: COUNTRIES_QUERY })
         }).then(response => response.json())
-        .then(data => setLaunches(data))
+        .then(data => setCountries(data.data.continent.countries))
     },[]);
     return(
         <div>
             <h1> Country API </h1>
-            <ul> 
-                {JSON.stringify(launches, null, 2)}
+            <ul>  
+            {/* {JSON.stringify(countries, null, 2)} */}
+                {countries.map(country => (
+                  <div>
+                    <h3>Państwo: {country.name}</h3>
+                    <span>Stolica: {country.capital}</span>
+                  </div>
+                ))}
             </ul>
         </div>
     )

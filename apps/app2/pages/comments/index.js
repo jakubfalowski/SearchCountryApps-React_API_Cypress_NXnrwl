f4ae6d7157b25fab54e829b7348a13451b97540f
jakubfalowski <kubafa99@gmail.com>
2022-03-31 13:19:44 +0200

    search with user query (not testing, max daily queries)

--- apps/app2/pages/comments/index.js
+++ apps/app2/pages/comments/index.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect} from "react";
 
 function CommentsPage(){
-    let [functionEnabled, enableFunction] = useState([])
-    let [comments, setComments] = useState([])
+    const [functionEnabled, enableFunction] = useState([])
+    const [comments, setComments] = useState([])
+    const [query, setQuery] = useState('')
+    console.log(query)
+    
     const fetchComments = async () => {
-        const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland')
+        const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q='+query)
         const data = await response.json()
         setComments(data)
         enableFunction(true)
     }
     return(
         <div>
-        <button onClick = {fetchComments}> Load comments </button>
-        { functionEnabled === true ? comments.queries.request.map((item, i) => {
-          return <div key={i}><h3>Wyszukiwane hasło: {item.searchTerms}</h3><span>Liczba wyszukań: {item.totalResults}</span></div>
-        }) : <p> Wpisz dowolną wartość </p>}
-        </div>
-        
-        
+            <input value={query} type="text" placeholder="google" onChange={e => setQuery(e.target.value)}/>
+            <button onClick = {fetchComments}> Wyświetl</button>
+            { functionEnabled === true ? comments.queries.request.map((item, i) => {
+            return <div key={i}><h3>Wyszukiwane hasło: {item.searchTerms}</h3><span>Liczba wyszukań: {item.totalResults === undefined ? 0 : item.totalResults}</span></div>
+            }) : <p> Musisz wpisać dowolną wartość </p>}
+        </div>    
     )
 }
 export default CommentsPage

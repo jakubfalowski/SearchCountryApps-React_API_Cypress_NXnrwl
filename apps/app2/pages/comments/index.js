e6c2315e182a952b3a11611cc76e4494dc421f19
jakubfalowski <kubafa99@gmail.com>
2022-03-31 11:58:20 +0200

    getInformationFromAPI

--- apps/app2/pages/comments/index.js
+++ apps/app2/pages/comments/index.js
@@ -1,62 +1,20 @@
 import React, { useState } from "react";
 
 function CommentsPage(){
+    let [functionEnabled, enableFunction] = useState([])
     let [comments, setComments] = useState([])
     const fetchComments = async () => {
         const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland')
         const data = await response.json()
         setComments(data)
+        enableFunction(true)
     }
-    
-    const sampleJSON = {
-        
-            "kind": "customsearch#search",
-            "url": {
-              "type": "application/json",
-              "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
-            },
-            "queries": {
-              "request": [
-                {
-                  "title": "Google Custom Search - poland",
-                  "totalResults": "14440000000",
-                  "searchTerms": "poland",
-                  "count": 10,
-                  "startIndex": 1,
-                  "inputEncoding": "utf8",
-                  "outputEncoding": "utf8",
-                  "safe": "off",
-                  "cx": "017576662512468239146:omuauf_lfve"
-                }
-              ],
-              "nextPage": [
-                {
-                  "title": "Google Custom Search - poland",
-                  "totalResults": "14440000000",
-                  "searchTerms": "poland",
-                  "count": 10,
-                  "startIndex": 11,
-                  "inputEncoding": "utf8",
-                  "outputEncoding": "utf8",
-                  "safe": "off",
-                  "cx": "017576662512468239146:omuauf_lfve"
-                }
-              ]
-            }
-            
-      }
-      console.log(sampleJSON.queries.request)
-      console.log(comments)
     return(
         <div>
         <button onClick = {fetchComments}> Load comments </button>
-        {comments.kind}
-        { sampleJSON.queries.request.map((item, i) => {
-          return <div key={i}><h3>{item.searchTerms}</h3><span>{item.totalResults}</span></div>
-        }) }
-        {/* { comments.queries.request.map((item, i) => {
-          return <div key={i}><h3>{item.searchTerms}</h3><span>{item.totalResults}</span></div>
-        }) } */}
+        { functionEnabled === true ? comments.queries.request.map((item, i) => {
+          return <div key={i}><h3>Wyszukiwane hasło: {item.searchTerms}</h3><span>Liczba wyszukań: {item.totalResults}</span></div>
+        }) : <p> Wpisz dowolną wartość </p>}
         </div>
         
         

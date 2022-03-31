730aecf79d886229e654efd6b1a5c98af2580753
jakubfalowski <kubafa99@gmail.com>
2022-03-31 11:51:30 +0200

    testing

--- apps/app2/pages/comments/index.js
+++ apps/app2/pages/comments/index.js
@@ -1,52 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 
-const sampleJSON = {
-    "kind": "customsearch#search",
-  "url": {
-    "type": "application/json",
-    "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
-  }  
-}
-
-const sampleJSON2 = {
-    "queries": {
-    "request": [
-        {
-          "title": "Google Custom Search - poland",
-          "totalResults": "14440000000",
-          "searchTerms": "poland",
-          "count": 10,
-          "startIndex": 1,
-          "inputEncoding": "utf8",
-          "outputEncoding": "utf8",
-          "safe": "off",
-          "cx": "017576662512468239146:omuauf_lfve"
-        }
-      ]
-  }
-}
-
-function CommentsPage() {
-
-  return (
-    <div>
-      {
-        Object.keys(sampleJSON.url).map((key, i) => (
-          <p key={i}>
-            <span>Key Name: {key}</span>
-            <span>Value: {sampleJSON.url[key]}</span>
-          </p>
-        ))
+function CommentsPage(){
+    let [comments, setComments] = useState([])
+    const fetchComments = async () => {
+        const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g&cx=017576662512468239146:omuauf_lfve&q=poland')
+        const data = await response.json()
+        setComments(data)
+    }
+    
+    const sampleJSON = {
+        
+            "kind": "customsearch#search",
+            "url": {
+              "type": "application/json",
+              "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+            },
+            "queries": {
+              "request": [
+                {
+                  "title": "Google Custom Search - poland",
+                  "totalResults": "14440000000",
+                  "searchTerms": "poland",
+                  "count": 10,
+                  "startIndex": 1,
+                  "inputEncoding": "utf8",
+                  "outputEncoding": "utf8",
+                  "safe": "off",
+                  "cx": "017576662512468239146:omuauf_lfve"
+                }
+              ],
+              "nextPage": [
+                {
+                  "title": "Google Custom Search - poland",
+                  "totalResults": "14440000000",
+                  "searchTerms": "poland",
+                  "count": 10,
+                  "startIndex": 11,
+                  "inputEncoding": "utf8",
+                  "outputEncoding": "utf8",
+                  "safe": "off",
+                  "cx": "017576662512468239146:omuauf_lfve"
+                }
+              ]
+            }
+            
       }
-
-      <h2>Array of Objects:</h2>
-      <ul>
-        {sampleJSON2.queries.request.map((item, i) => {
-          return <li key={i}>{item.searchTerms} - {item.totalResults}</li>
-        })}
-      </ul>
-    </div>
-  )
+      console.log(sampleJSON.queries.request)
+      console.log(comments)
+    return(
+        <div>
+        <button onClick = {fetchComments}> Load comments </button>
+        {comments.kind}
+        { sampleJSON.queries.request.map((item, i) => {
+          return <div key={i}><h3>{item.searchTerms}</h3><span>{item.totalResults}</span></div>
+        }) }
+        {/* { comments.queries.request.map((item, i) => {
+          return <div key={i}><h3>{item.searchTerms}</h3><span>{item.totalResults}</span></div>
+        }) } */}
+        </div>
+        
+        
+    )
 }
 export default CommentsPage

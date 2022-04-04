59a7ad01fbc42f608530c23482c3f875e53842a3
jakubfalowski <kubafa99@gmail.com>
2022-04-04 14:05:15 +0200

    test: limitation

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@app/header';
+import { SearchGoogle } from '@app/search-google';
 import { SelectCountries } from '@app/select-countries';
 
 export function App() {
   return (
     <div>
       <Header api='GraphQL - Country' nxname='app' />
-      <SelectCountries />
+      <SelectCountries /><SearchGoogle />
       </div>
   );
 }

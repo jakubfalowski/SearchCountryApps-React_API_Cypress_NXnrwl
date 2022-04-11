79438452f9f8c413ff8215dc11879694c0f9e882
jakubfalowski <kubafa99@gmail.com>
2022-04-11 10:46:04 +0200

    tests: jest app2

--- apps/app/src/app/app.spec.tsx
+++ apps/app/src/app/app.spec.tsx
@@ -1,31 +1,14 @@
-// import { render } from '@testing-library/react';
-
-// import App from './app';
-
-// describe('App', () => {
-//   it('Powinno się wyrenderować', () => {
-//     const { baseElement } = render(<App />);
-
-//     expect(baseElement).toBeTruthy();
-//   });
-
-//   it('Powinno być imie i nazwisko autora', () => {
-//     const { getByText } = render(<App />);
-
-//     expect(getByText(/Jakub Fałowski/)).toBeTruthy();
-//   });
-// });
 import ReactDOM from 'react-dom';
 import {ContinentButton} from '@app/web/data-access-countries'
 import {render} from '@testing-library/react'
 import "@testing-library/jest-dom/extend-expect"
 
-it("Prawidłowo wyrenderowane", ()=>{
+it("Prawidłowo wyrenderowane(react-dom)", ()=>{
   const span = document.createElement("span");
   ReactDOM.render(<ContinentButton continent="Europe" continentCode="EU" />, span)
 })
 
-it("Prawidłowy button", () => {
+it("Prawidłowo wyrenderowane button(jest-dom)", () => {
   const {getByTestId} = render(<ContinentButton continent="Europe" continentCode="EU"/>);
   expect(getByTestId('button')).toHaveTextContent("Europe")
 })
--- apps/app2/specs/index.spec.tsx
+++ apps/app2/specs/index.spec.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import CustomApp from '../pages/_app';
 
-describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<CustomApp />);
+import App from '../pages/_app';
+
+describe('App', () => {
+  it('Powinno się wyrenderować', () => {
+    const { baseElement } = render(<App />);
+
     expect(baseElement).toBeTruthy();
   });
+
+  it('Powinno być imie i nazwisko autora', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText(/Jakub Fałowski/)).toBeTruthy();
+  });
+
+  it('4 dodać 6 powinno być większe od 7', () => {
+    function sum(a, b) {
+      return a + b;
+    }
+    expect(sum(4, 6)).toBeGreaterThan(7)
+  });
+
 });

548cf39a222d43e5d33144e98f46614e19cdff54
jakubfalowski <kubafa99@gmail.com>
2022-04-15 12:21:04 +0200

    fix: tables country

--- libs/web/data-access-countries/src/lib/BasicTable.tsx
+++ libs/web/data-access-countries/src/lib/BasicTable.tsx
@@ -1,15 +1,15 @@
-import React, {useMemo} from 'react'
-import { useTable } from 'react-table'
-import { COLUMNS } from './columns'
-import {MOCK_DATA} from './MOCK_DATA' 
+// import React, {useMemo} from 'react'
+// import { useTable } from 'react-table'
+// import { COLUMNS } from './columns'
+// import { MOCK_DATA } from './MOCK_DATA' 
 
-export const BasicTable = () => {
-    const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => MOCK_DATA, [])
+// export const BasicTable = () => {
+//     const columns = useMemo(() => COLUMNS, [])
+//     const data = useMemo(() => MOCK_DATA, [])
 
-    useTable({
-        columns,
-        data
-    })
-}
-// https://www.youtube.com/watch?v=hson9BXU9F8
+//     useTable({
+//         columns,
+//         data
+//     })
+// }
+// // https://www.youtube.com/watch?v=hson9BXU9F8
--- libs/web/data-access-countries/src/lib/CountryAPI.tsx
+++ libs/web/data-access-countries/src/lib/CountryAPI.tsx
@@ -10,32 +10,56 @@ export default function CountryAPI() {
       name
       countries{
         name
+        native
+        code
         capital
+        currency
+        phone
       }
     }
   }`;
   const fetchURL = 'https://countries.trevorblades.com/graphql';
-
-  if (codeContinent !== '') {
-    React.useEffect(() => {
-      fetch(fetchURL, {
-        method: 'POST',
+  const options = {
+    method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query: COUNTRIES_QUERY }),
-      })
-        .then((response) => response.json())
-        .then((data) => setCountries(data.data.continent.countries));
-    }, []);
+    }
+
+  const fetchData = async () => {
+    try {
+        const response = await fetch(fetchURL + COUNTRIES_QUERY, options);
+        const data = await response.json();
+        setCountries(data.data.continent.countries)
+    } catch (err) {
+        console.error(err);
+    }
   }
+  if (codeContinent !== '') fetchData()
 
   return (
     <div>
-      {countries.map((country, i) => (
-        <ul key={i}>
-          <li>Państwo: {country.name}</li>
-          <li>Stolica: {country.capital === null ? '-' : country.capital}</li>
-        </ul>
+      <table>
+        <tbody>
+          <tr>
+            <th>Kraj</th>
+            <th>Nazwa kraju w ich języku</th>
+            <th>Kod</th>
+            <th>Stolica</th>
+            <th>Waluta</th>
+            <th>Telefon</th>
+          </tr>
+        </tbody>
+      {countries.map(country => (
+        <tr key={country.code}>
+          <td>{country.name}</td>
+          <td>{country.native}</td>
+          <td>{country.code}</td>
+          <td>{country.capital === null ? '-' : country.capital}</td>
+          <td>{country.currency}</td>
+          <td>{country.phone}</td>
+        </tr>
       ))}
+      </table>
     </div>
   );
 }

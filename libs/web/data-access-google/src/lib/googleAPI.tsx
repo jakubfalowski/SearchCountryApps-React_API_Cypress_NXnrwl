5c7be0893619c0376e8b7c772bced6407ba71329
jakubfalowski <kubafa99@gmail.com>
2022-04-28 12:52:48 +0200

    feat: map jsx elements from input

--- libs/web/data-access-google/src/lib/googleAPI.tsx
+++ libs/web/data-access-google/src/lib/googleAPI.tsx
@@ -1,30 +1,76 @@
-import { useState } from 'react';
-import { Group, Button, Grid, TextInput, Box } from '@mantine/core';
+import React, { useState } from 'react';
+import { Group, Button, Grid, TextInput, Box, NumberInput } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import { ResponsiveContainer, PieChart, Pie, Tooltip } from 'recharts';
 import { useForm } from '@mantine/form';
 
 export function GoogleAPI() {
-  const [results1, setResults1] = useState(0);
-  const [results2, setResults2] = useState(0);
-  const [firstQuery, setFirstQuery] = useState('');
-  const [secondQuery, setSecondQuery] = useState('');
+  const [totalResults, setTotalResults] = useState([] as any)
+  const [searchTimes, setSearchTimes] = useState([] as any)
+  const [query, setQuery] = useState([] as any)
+  const [numberOfQueries, setNumberOfQueries] = useState(1);
   const key='AIzaSyC9ntEwOZg7dixTbfbVOTLr3YNx6fvOI4g';
+  const key2='AIzaSyB45fm5hOp9Fpm-1z9ACUfrLVLQKTuMWBY'
   const fetchURL =
     `https://www.googleapis.com/customsearch/v1?key=${key}&cx=017576662512468239146:omuauf_lfve&q=`;
 
-  const fetchResults = async (searchName1: string, searchName2: string) => {
-    const response1 = await fetch(fetchURL + searchName1);
-    const response2 = await fetch(fetchURL + searchName2);
-    const data1 = await response1.json();
-    const data2 = await response2.json();
-    setResults1(parseInt(data1.searchInformation.totalResults));
-    setResults2(parseInt(data2.searchInformation.totalResults));
-    
+  const fetchResults = async (searchNames: Array<string>, numberOfQueries: number) => {
+    const response = []
+    const data = []
+    for(let i = 0; i<numberOfQueries; i++){
+      response[i] = await fetch(fetchURL + searchNames[i]);
+      data[i] = await response[i].json();
+      setTotalResults(data[i].searchInformation.totalResults)
+      setSearchTimes(data[i].searchInformation.searchTimes)
+    }
   };
+  
+  function returnButton(i:number){
+    return [...Array(i).keys()].map(x => {
+      return(
+        <Button
+          variant="outline"
+          onClick={() =>
+            showNotification({
+              title: query[x],
+              message: `wyniki wyszukiwania: ${totalResults[x]}, czas wyszukania: ${searchTimes[x]}`,
+            })
+          }
+        >
+          {x+1} : {query[x]}
+        </Button>
+      )
+    })
+  }
 
-  console.log(typeof(results1));
-  console.log(results2);
+  function returnInput(i:number){
+    return [...Array(i).keys()].map(x => {
+      return(
+        <TextInput
+          required
+          label={`Zapytanie nr. ${x+1}`}
+          placeholder="google query"
+          {...form.getInputProps('input1')}
+        />
+      )
+    })
+  }
+
+  // function PieData(i: number){
+  //   for(let x = 0; x<i; x++){
+  //     {name: query[x], value: totalResults[x]}
+  //   }
+  // } 
+
+  const number = useForm({
+    initialValues: {
+      input1: '',
+    },
+
+    validate: {
+      input1: (value) => (/^[0-9]+$/.test(value) ? null : 'Wprowadziłeś nieprawidłowe dane. Wymagane litery bądź spacje.'),
+    },
+  });
 
   const form = useForm({
     initialValues: {
@@ -41,24 +87,16 @@ export function GoogleAPI() {
   return (
     
     <div>
+      <form onSubmit={number.onSubmit((function(values) {returnButton(numberOfQueries); returnInput(numberOfQueries); }))}>
+        <NumberInput value={numberOfQueries} max={5} min={1} onChange={(val: number) => setNumberOfQueries(val)} />
+      </form>
       <h3>
         Wpisz dane wyszukiwanie, by sprawdzić jak często zostało one wyświetlane
       </h3>
+      {returnButton(numberOfQueries)}
       <Box sx={{ maxWidth: 300 }} mx="auto">
-      <form onSubmit={form.onSubmit((function(values) { fetchResults(values.input1, values.input2); setFirstQuery(values.input1); setSecondQuery(values.input2);}))} >
-        <TextInput
-          required
-          label="Pierwsze zapytanie"
-          placeholder="poland"
-          {...form.getInputProps('input1')}
-        />
-
-        <TextInput
-          required
-          label="Drugie zapytanie"
-          placeholder="england"
-          {...form.getInputProps('input2')}
-        />
+      <form /* onSubmit={form.onSubmit((function(values) { fetchResults(values.input1, values.input2); setFirstQuery(values.input1); setSecondQuery(values.input2);}))} */>
+        {returnInput(numberOfQueries)}
 
         <Group position="right" mt="md">
           <Button type="submit">Submit</Button>
@@ -67,47 +105,22 @@ export function GoogleAPI() {
     </Box>
     
     <Grid>
-          <Grid.Col span={4}>
-            
-          
-      {results1 !==0 || results2 !== 0 ? 
-      <div><div style={{ width: '100%', height: 300 }}>
-      <ResponsiveContainer>
-      <PieChart>
-        <Pie dataKey="value" data={[
-          { name: firstQuery+'', value: results1+0 },
-          { name: secondQuery+'', value: results2+0 },
-          ]} fill="#8884d8" label />
-          <Tooltip />
-      </PieChart>
-    </ResponsiveContainer></div>
-    <Button
-        variant="outline"
-        onClick={() =>
-          showNotification({
-            title: firstQuery,
-            message: results1,
-          })
+      <Grid.Col span={4}>   
+        {totalResults[0] !== 0 ? 
+        <div>
+        <div style={{ width: '100%', height: 300 }}>
+          <ResponsiveContainer>
+            <PieChart>
+              {/* <Pie dataKey="value" data={PieData} fill="#8884d8" label /> */}
+                <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        </div>
+        : <p> Nie znaleziono </p>
         }
-      >
-        {firstQuery}
-      </Button>
-      <Button
-        variant="outline"
-        onClick={() =>
-          showNotification({
-            title: secondQuery,
-            message: results2,
-          })
-        }
-      >
-        {secondQuery}
-      </Button>
-    </div>
-     : <p> Nie znaleziono </p>
-     }
-     </Grid.Col>
-        </Grid>
+      </Grid.Col>
+    </Grid>
       {/* {functionEnabled === true ? (
         results.queries.request.map((item: any, i: number) => {
           return (
